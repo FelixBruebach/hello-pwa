a8616623a996e4e3796ee2813bb7b07a54136eee
Felix Brübach <felixbruebach@outlook.de>
2021-01-09 15:58:46 +0100

    bulk

--- README.md
+++ README.md
@@ -1,25 +1,11 @@
-# Hello PWA
-The classic Hello World program as a Progressive Web App
+# MSEContactSharePWA
 
-![Hello World Screenshot](./screenshot.jpg "Hello World Screenshot")
-
-## What It Is
-
-A simple PWA demo that displays the text "Hello, World!" utilizing an app manifest, service workers and caching. This demo is written in plain-vanilla HTML, CSS and Javascript (ES6). It has no dependencies.
-
-I've also written a tutorial to go along with the code. Read it on Medium:
-
-https://medium.com/james-johnson/a-simple-progressive-web-app-tutorial-f9708e5f2605
+A simple proof of concept PWA that allows the user to select a contact and share it with another app e.g. WhatsApp via the Contact Picker and Web Share API. Tested on Android.
 
 ## Demo
 
-https://jamesjohnson280.github.io/hello-pwa/
+https://felixbruebach.github.io/hello-pwa/
 
-## Browser Compatibility
+![MSEContactSharePWA Screenshot](./screenshot.png "MSEContactSharePWA Screenshot")
 
-Hello PWA has been tested in the following browsers:
 
-* Chrome 67 (Windows and Android)
-* Firefox 60
-* Safari 11
-* Edge 42
--- js/main.js
+++ js/main.js
@@ -1,8 +1,67 @@
-window.onload = () => {
-  'use strict';
-
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
-  }
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js");
 }
+
+let contactInformation;
+let profilePicture;
+
+const contactImage = document.getElementById("contact-image");
+const contactInformationText = document.getElementById("contact-information-text");
+const selectButton = document.getElementById("choose-button");
+const shareButton = document.getElementById("share-button");
+
+selectButton.addEventListener("click", async function () {
+    if ("contacts" in navigator && "ContactsManager" in window) {
+        const properties = ["name", "email", "tel", "address", "icon"];
+        const options = {multiple: false};
+
+        const contact = (await navigator.contacts.select(properties, options))[0];
+
+        contactInformation = undefined;
+        profilePicture = undefined;
+
+        contactInformationText.value = "";
+        contactImage.src = "";
+
+        const name = contact.name[0];
+        const email = contact.email[0];
+        const tel = contact.tel[0];
+        const address = contact.address[0];
+        const icon = contact.icon[0];
+
+        if (name) contactInformation += `Name: ${name}\n`;
+        if (email) contactInformation += `E-Mail: ${email}\n`;
+        if (tel) contactInformation += `Nummer: ${tel}\n`;
+        if (address) contactInformation += `Adresse: ${address}\n`;
+        contactInformation += "\n";
+        if (icon) profilePicture = new File([icon], `${name}.jpg`, {type: "image/jpeg"});
+
+        contactInformationText.innerHTML = contactInformation.replace("\n", "<br>");
+
+        if (profilePicture) {
+            const fr = new FileReader();
+            fr.onload = () => contactImage.src = fr.result;
+            fr.readAsDataURL(profilePicture);
+        }
+
+        shareButton.style.display = "block";
+    } else {
+        contactInformationText.value = "Contact Picker API is not supported.";
+    }
+});
+
+shareButton.addEventListener("click", async function () {
+    if (navigator.share) {
+        navigator.share({
+            text: contactInformation,
+            files: profilePicture ? [profilePicture] : undefined,
+        })
+            .then(() => console.log("Successful share"))
+            .catch((error) => console.log("Error sharing", error));
+    } else {
+        contactImage.value = "Web Share API is not supported.";
+    }
+});
+
+
+
